bbde16d0994ae4d2812b31a9fd3798e56ea1d723
Brinenas <51021984+Brinenas@users.noreply.github.com>
2025-01-01 03:52:57 +0800

    fix Incorrect timeout error when using FindStringMatch() (#86)
    * fix Timeout Error issue: when the fastclock stop running, then next matches will get a wrong fast.current and daeline, thus trigger an incorrect timeout error.
    
    Co-authored-by: ccoVeille <3875889+ccoVeille@users.noreply.github.com>
    Co-authored-by: hezhipeng.09 <hezhipeng.09@bytedance.com>
    Co-authored-by: Doug Clark <dlclark@gmail.com>

--- fastclock_test.go
+++ fastclock_test.go
@@ -2,6 +2,7 @@ package regexp2
 
 import (
 	"fmt"
+	"log"
 	"testing"
 	"time"
 )
@@ -54,5 +55,60 @@ func TestStopTimeoutClock(t *testing.T) {
 	}
 	if want, got := false, fast.running; want != got {
 		t.Errorf("Expected isRunning to be %v, got %v", want, got)
+	}
+}
+func TestIncorrectDeadline(t *testing.T) {
+	if fast.start.IsZero() {
+		fast.start = time.Now()
+	}
+	// make fast stopped
+	for fast.running {
+		time.Sleep(clockPeriod)
+	}
+	t.Logf("current fast: %+v", fast)
+	timeout := 5 * clockPeriod
+	// make the error time much bigger
+	time.Sleep(10 * clockPeriod)
+	nowTick := durationToTicks(time.Since(fast.start))
+	// before fix, fast.current will be the time fast stopped, and end is incorrect too
+	// after fix, fast.current will be current time.
+	d := makeDeadline(timeout)
+	gotTick := fast.current.read()
+	t.Logf("nowTick: %+v, gotTick: %+v", nowTick, gotTick)
+	if nowTick > gotTick {
+		t.Errorf("Expectd current should greater than %v, got %v", gotTick, nowTick)
+	}
+	expectedDeadTick := nowTick + durationToTicks(timeout)
+	if d < expectedDeadTick {
+		t.Errorf("Expectd deadTick %v, got %v", expectedDeadTick, d)
+	}
+}
+
+func TestIncorrectTimeoutError(t *testing.T) {
+	log.SetFlags(log.Lmicroseconds)
+	re := MustCompile(`\[(\d+)\]\s+\[([\s\S]+)\]\s+([\s\S]+).*`, RE2)
+	// there's a lot of slop in the timeout process (on purpose to keep resources low)
+	// need a timeout of at least 5x the clockPeriod to have consistent test behavior
+	re.MatchTimeout = 5 * clockPeriod
+
+	_, err := re.FindStringMatch("[10000] [Dec 15, 2012 1:42:43 AM] com.dev.log.LoggingExample main")
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	// now wait - this makes sure the background timer goroutine is stopped
+	time.Sleep(time.Second + re.MatchTimeout*2)
+
+	if val := fast.clockEnd.read() - fast.current.read(); val > 0 {
+		t.Fatalf("unexpected bg timer running: %v", val)
+	}
+
+	// each of these should be plenty fast to not timeout
+	for i := 0; i < 1000; i++ {
+		_, err := re.FindStringMatch("[10000] [Dec 15, 2012 1:42:43 AM] com.dev.log.LoggingExample main")
+		if err != nil {
+			log.Printf("timeout")
+			t.Fatalf("Expecting no error, got: '%v' on iteration %v", err, i)
+		}
 	}
 }
